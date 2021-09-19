9884e316e052d86907b72477d64fbf769ed26cc3
ianaf1 <ianfatah1@gmail.com>
2021-09-19 08:11:09 +0700

    Update footer.php

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 2.3.0
     </div>
-    <strong> &copy; 2019 <a href="#">Absensi Karyawan</a>.</strong> Template by Admin LTE.
+    <strong> &copy; 2021 <a href="#">Absensi Siswa</a>.</strong> MA AT-TAQWA YASTU.
 </footer>
